a1a704c0795fd179ae69f9d35fe31f7dfc292ce2
mdashraful24 <mdashrafulislam2882@gmail.com>
2024-10-06 13:43:48 +0600

    added adopt section grid part

--- sa-index-3.js
+++ sa-index-3.js
@@ -35,23 +35,42 @@ const showModalInfo = (petData) => {
     card.classList = 'card card-compact';
     card.innerHTML = `
         <div class="">
-                    <figure class ="h-[200px] w-[300px] relative">
-                      <img
-                        src= ${petData.image}
-                        alt="Pets" />
-                    </figure>
-                    <div class="">
-                      <h2 class="card-title">${petData.pet_name}</h2>
-                      <p>Breed: ${petData.breed}</p>
-                      <p>Birth: ${petData.date_of_birth}</p>
-                      <p>Gender: ${petData.gender}</p>
-                      <p>Price: ${petData.price}</p>
-                    </div>
-                    <div class="">
-                      <h2 class="text-xl font-bold">Details Information</h2>
-                      <p>${petData.pet_details}</p>
-                    </div>
-                  </div>
+  <div class="card space-y-4">
+    <div class="">
+      <img src=${petData.image} class="rounded-xl w-full" alt="">
+    </div>
+    <div>
+      <div class="space-y-2">
+        <h2 class="card-title">${petData.pet_name}</h2>
+        <div class="flex items-center gap-1">
+          <img src="./assets/frame1.png" alt="">
+          <p>Breed: ${petData.breed}</p>
+        </div>
+        <div class="flex items-center gap-1">
+          <img src="./assets/frame2.png" alt="">
+          <p>Birth: ${petData.date_of_birth}</p>
+        </div>
+        <div class="flex items-center gap-1">
+          <img src="./assets/frame3.png" alt="">
+          <p>Gender: ${petData.gender}</p>
+        </div>
+        <div class="flex items-center gap-1">
+          <img src="./assets/frame4.png" alt="">
+          <p>Price: ${petData.price}$</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="text-justify space-y-2">
+        <h2 class="text-xl font-bold">Details Information</h2>
+        <p>${petData.pet_details}</p>
+      </div>
+    </div>
+    <div>
+      <button
+        class="btn w-full bg-gray-200 btn-md hover:bg-primary hover:text-white transition-colors ease-in duration-100 text-lg">Close</button>
+    </div>
+  </div>
         `;
     document.getElementById('modal-container').append(card);
     // console.log(petData);
@@ -62,7 +81,10 @@ const showModalInfo = (petData) => {
 //like images
 const likeImage = (petData) => {
     document.getElementById('liked-images').innerHTML = `
-    <img class="h-[200px] w-[300px] p-3" src= ${petData.image} alt="Pets" />`;
+    <div class="p-3">
+  <img class="h-[190px] rounded-xl" src=${petData.image} alt="Pets" />
+</div>
+    `;
 }
 //changing pets
 document.getElementById('cat').addEventListener('click', function () {
@@ -108,37 +130,37 @@ const displayPets = (pets) => {
         const card = document.createElement('div');
         card.classList = 'card card-compact';
         card.innerHTML = `
-        <div class="card card-compact bg-base-100 w-96 mx-auto shadow-xl p-5 space-y-3">
-                    <div>
-                        <img class="rounded-xl min-w-full" src=${pet.image} alt="" />
-                    </div>
-                    <div class="space-y-3">
-                        <div class="space-y-2">
-                        <h2 class="card-title">${pet.pet_name}</h2>
-                        <div class="flex items-center gap-1">
-                            <img src="./assets/frame1.png" alt="">
-                            <p>Breed: ${pet.breed}</p>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <img src="./assets/frame2.png" alt="">
-                            <p>Birth: ${pet.date_of_birth}</p>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <img src="./assets/frame3.png" alt="">
-                            <p>Gender: ${pet.gender}</p>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <img src="./assets/frame4.png" alt="">
-                            <p>Price: ${pet.price}$</p>
-                        </div>
-                        </div>
-                        <div class="card-actions justify-between gap-3">
-                        <button onclick="loadImages(${pet.petId})" class="btn">Like</button>
-                        <button class="btn">Adopt</button>
-                        <button onclick="loadModalInfo(${pet.petId})" class="btn">Details</button>
-                        </div>
-                    </div>
-                    </div>
+        <div class="p-3">
+  <div class="card shadow p-3">
+    <div class="px-3 pt-3"><img src=${pet.image} class="rounded-xl" alt=""></div>
+    <div class="card-body">
+      <div class="space-y-2">
+      <h2 class="card-title">${pet.pet_name}</h2>
+      <div class="flex items-center gap-1">
+        <img src="./assets/frame1.png" alt="">
+        <p>Breed: ${pet.breed}</p>
+      </div>
+      <div class="flex items-center gap-1">
+        <img src="./assets/frame2.png" alt="">
+        <p>Birth: ${pet.date_of_birth}</p>
+      </div>
+      <div class="flex items-center gap-1">
+        <img src="./assets/frame3.png" alt="">
+        <p>Gender: ${pet.gender}</p>
+      </div>
+      <div class="flex items-center gap-1">
+        <img src="./assets/frame4.png" alt="">
+        <p>Price: ${pet.price}$</p>
+      </div>
+    </div>
+      <div class="card-actions justify-between">
+      <button onclick="loadImages(${pet.petId})" class="btn">Like</button>
+      <button class="btn">Adopt</button>
+      <button onclick="loadModalInfo(${pet.petId})" class="btn">Details</button>
+    </div>
+    </div>
+  </div>
+</div>
         `
         petsContainer.append(card);
 
@@ -165,52 +187,52 @@ loadPets();
 // }
 
 
-[
-    {
-        "petId": 2,
-        "breed": "Siamese",
-        "category": "Cat",
-        "date_of_birth": "2022-09-05",
-        "price": 800,
-        "image": "https://i.ibb.co.com/3Wzz41D/pet-2.jpg",
-        "gender": "Female",
-        "pet_details": "This affectionate female Siamese cat is known for her vocal nature and love for attention. Born on September 5, 2022, she enjoys interactive play and snuggles. Fully vaccinated and priced at $800, she's the perfect fit for cat lovers who appreciate an intelligent, engaging, and sociable feline companion.",
-        "vaccinated_status": "Fully",
-        "pet_name": "Mia"
-    },
-    {
-        "petId": 6,
-        "breed": "Bengal",
-        "category": "Cat",
-        "price": 950,
-        "image": "https://i.ibb.co.com/PFbWMGk/pet-6.jpg",
-        "gender": "Male",
-        "pet_details": "This playful male Bengal cat, born on November 10, 2022, is full of energy and loves to climb and engage with toys. Fully vaccinated and priced at $950, he's ideal for active households looking for a curious and adventurous feline friend.",
-        "vaccinated_status": "Fully",
-        "pet_name": "Leo"
-    },
-    {
-        "petId": 7,
-        "breed": "Bengal",
-        "category": "Cat",
-        "date_of_birth": "2022-11-10",
-        "price": 950,
-        "image": "https://i.ibb.co.com/QXbXctF/pet-7.jpg",
-        "gender": "Male",
-        "pet_details": "This male Bengal cat, born on November 10, 2022, is energetic and playful. He loves exploring, climbing, and playing with interactive toys. Fully vaccinated and priced at $950, he's perfect for anyone looking for an active, intelligent, and lively cat.",
-        "vaccinated_status": null,
-        "pet_name": "Max"
-    },
-    {
-        "petId": 17,
-        "breed": "Maine Coon",
-        "category": "Cat",
-        "date_of_birth": "2022-12-01",
-        "price": 1200,
-        "image": "https://i.ibb.co.com/85w4kSt/pet-17.jpg",
-        "gender": "Male",
-        "pet_details": "This majestic male Maine Coon, born on December 1, 2022, is known for his gentle demeanor and friendly personality. Fully vaccinated and priced at $1200, he's great with families and other pets.",
-        "vaccinated_status": "Fully",
-        "pet_name": "Thor"
-    }
-]
+// [
+//     {
+//         "petId": 2,
+//         "breed": "Siamese",
+//         "category": "Cat",
+//         "date_of_birth": "2022-09-05",
+//         "price": 800,
+//         "image": "https://i.ibb.co.com/3Wzz41D/pet-2.jpg",
+//         "gender": "Female",
+//         "pet_details": "This affectionate female Siamese cat is known for her vocal nature and love for attention. Born on September 5, 2022, she enjoys interactive play and snuggles. Fully vaccinated and priced at $800, she's the perfect fit for cat lovers who appreciate an intelligent, engaging, and sociable feline companion.",
+//         "vaccinated_status": "Fully",
+//         "pet_name": "Mia"
+//     },
+//     {
+//         "petId": 6,
+//         "breed": "Bengal",
+//         "category": "Cat",
+//         "price": 950,
+//         "image": "https://i.ibb.co.com/PFbWMGk/pet-6.jpg",
+//         "gender": "Male",
+//         "pet_details": "This playful male Bengal cat, born on November 10, 2022, is full of energy and loves to climb and engage with toys. Fully vaccinated and priced at $950, he's ideal for active households looking for a curious and adventurous feline friend.",
+//         "vaccinated_status": "Fully",
+//         "pet_name": "Leo"
+//     },
+//     {
+//         "petId": 7,
+//         "breed": "Bengal",
+//         "category": "Cat",
+//         "date_of_birth": "2022-11-10",
+//         "price": 950,
+//         "image": "https://i.ibb.co.com/QXbXctF/pet-7.jpg",
+//         "gender": "Male",
+//         "pet_details": "This male Bengal cat, born on November 10, 2022, is energetic and playful. He loves exploring, climbing, and playing with interactive toys. Fully vaccinated and priced at $950, he's perfect for anyone looking for an active, intelligent, and lively cat.",
+//         "vaccinated_status": null,
+//         "pet_name": "Max"
+//     },
+//     {
+//         "petId": 17,
+//         "breed": "Maine Coon",
+//         "category": "Cat",
+//         "date_of_birth": "2022-12-01",
+//         "price": 1200,
+//         "image": "https://i.ibb.co.com/85w4kSt/pet-17.jpg",
+//         "gender": "Male",
+//         "pet_details": "This majestic male Maine Coon, born on December 1, 2022, is known for his gentle demeanor and friendly personality. Fully vaccinated and priced at $1200, he's great with families and other pets.",
+//         "vaccinated_status": "Fully",
+//         "pet_name": "Thor"
+//     }
+// ]
